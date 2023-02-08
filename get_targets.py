487a9b2a1f2d14ed21670441cb8894feab8c682d
htaylan <htaylanturan@gmail.com>
2023-02-08 11:49:48 +0100

    Update get_targets.py

--- get_targets.py
+++ get_targets.py
@@ -1,31 +1,34 @@
 from chembl_webresource_client.new_client import new_client
 
-class TargetGetter:
-  def get_targets(target_name):
-      """
-      :param target_name: The name of the inhibitor target
-      :return: This function gets targets from ChemBL database
-      and return as a dataframe
-      """
-      self.target = new_client.target
-      self.target_query = target.search('target_name')
-      self.targets = pd.DataFrame.from_dict(target_query)
-      return targets
+class getChemBl:
+    def __init__(self):
+        self.new_client = Client(config.CHEMBL_API_KEY)
 
-  def select_target(n):
-      """
-      :return: Selects the target
-      """
-      self.selected_target = targets.target_chembl_id[0]
-      return selected_target
+    def get_targets(self, target_name):
+        """
+        :param target_name: The name of the inhibitor target
+        :return: This function gets targets from ChemBL database
+        and return as a dataframe
+        """
+        target = self.new_client.target
+        target_query = target.search(target_name)
+        targets = pd.DataFrame.from_dict(target_query)
+        return targets
 
-  def get_inhibitors(selected_target):
-      """
-      :param selected_target: ChemBl Id of selected target
-      :return: The inhibitors for selected target
-      from ChemBl database as a dataframe
-      """
-      self.activity = new_client.activity
-      res = activity.filter(target_chembl_id=selected_target)
-      df = pd.DataFrame.from_dict(res)
-      return df
+    def select_target(self, n):
+        """
+        :return: Selects the target
+        """
+        selected_target = self.targets.target_chembl_id[n]
+        return selected_target
+
+    def get_inhibitors(self, selected_target):
+        """
+        :param selected_target: ChemBl Id of selected target
+        :return: The inhibitors for selected target
+        from ChemBl database as a dataframe
+        """
+        activity = self.new_client.activity
+        res = activity.filter(target_chembl_id=selected_target)
+        df = pd.DataFrame.from_dict(res)
+        return df
