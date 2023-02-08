7084aa21d313b64258ced7709d450f639d026d26
htaylan <htaylanturan@gmail.com>
2023-02-08 12:00:08 +0100

    Update screening.py

--- screening.py
+++ screening.py
@@ -1,101 +1,87 @@
-def select_potency_type(df,pot_type):
-    """
-    :parameter pot_type: type of potency
-    :return dataframe based on selected potency type and drop NaN :
-    """
-    valid_types = 'IC50', 'EC50', 'Kd'
-    if pot_type not in valid_types:
-        raise ValueError("Type should be one of valid types")
+import pandas as pd
+import numpy as np
+import rdkit.Chem as Chem
+from rdkit import Descriptors
 
-    df.drop(df[(df.type != pot_type)].index, axis=0, inplace=True)
-    df = df[df.standard_value.notna()]
-    df = df.reset_index(drop=True)
-    return df
+class DataProcessor:
+    def __init__(self, df, pot_type, nec_columns, in_limit, ac_limit, smiles):
+        self.df = df
+        self.pot_type = pot_type
+        self.nec_columns = nec_columns
+        self.in_limit = in_limit
+        self.ac_limit = ac_limit
+        self.smiles = smiles
 
-def select_nec_columns(df,liste):
-    """
-    :return Select only necessary columns:
-    """
-    df = df[liste]
-    return df
+    def select_potency_type(self):
+        """
+        :return dataframe based on selected potency type and drop NaN :
+        """
+        valid_types = 'IC50', 'EC50', 'Kd'
+        if self.pot_type not in valid_types:
+            raise ValueError("Type should be one of valid types")
 
-def calculate_bioactivity(df,in_limit,ac_limit):
-    """
-    :return Calculate bioactivity (ac_limit), inactivity (in_limit)
-     and intermediate activity
-     based on standard_values. Add those standard values to the dataframe
-     as a new column called bioactivity_class :
-    """
+        self.df.drop(self.df[(self.df.type != self.pot_type)].index, axis=0, inplace=True)
+        self.df = self.df[self.df.standard_value.notna()]
+        self.df = self.df.reset_index(drop=True)
+        return self.df
 
-    df.standard_value = df.standard_value.astype("float")
-    bioactivity_class = []
-    for i in df['standard_value']:
-        if float(i) >= in_limit:
-            bioactivity_class.append('inactive')
-        elif float(i) <= ac_limit:
-            bioactivity_class.append('active')
-        else:
-            bioactivity_class.append('intermediate')
+    def select_nec_columns(self):
+        """
+        :return Select only necessary columns:
+        """
+        self.df = self.df[self.nec_columns]
+        return self.df
 
-    print(len(bioactivity_class))
-    df = pd.concat([df, pd.Series(bioactivity_class)], axis=1)
-    df.rename({0: "bioactivity_class"}, axis=1, inplace=True)
-    return df
+    def calculate_bioactivity(self):
+        """
+        :return Calculate bioactivity (ac_limit), inactivity (in_limit)
+        and intermediate activity
+        based on standard_values. Add those standard values to the dataframe
+        as a new column called bioactivity_class :
+        """
+        self.df.standard_value = self.df.standard_value.astype("float")
+        bioactivity_class = []
+        for i in self.df['standard_value']:
+            if float(i) >= self.in_limit:
+                bioactivity_class.append('inactive')
+            elif float(i) <= self.ac_limit:
+                bioactivity_class.append('active')
+            else:
+                bioactivity_class.append('intermediate')
 
+        self.df = pd.concat([self.df, pd.Series(bioactivity_class)], axis=1)
+        self.df.rename({0: "bioactivity_class"}, axis=1, inplace=True)
+        return self.df
+    
+    def lipinski(self):
+        for elem in self.smiles:
+            mol = Chem.MolFromSmiles(elem)
+            self.moldata.append(mol)
+        
+        for mol in self.moldata:
+            desc_MolWt = Descriptors.MolWt(mol)
+            desc_MolLogP = Descriptors.MolLogP(mol)
+            desc_NumHDonors = Descriptors.NumHDonors(mol)
+            desc_NumHAcceptor = Descriptors.NumHAcceptors(mol)
 
-def lipinski(smiles, verbose=False):
-    """
-    :param smiles: The smiles strings of the molecules
-    :param verbose: To not explicitly write descriptors to the screen
-    :return: Molecular Weight, LogP, H donors, and H acceptors descriptors
-    of each molecule as np.array then generates a dataframe
-    """
-    moldata = []
-    for elem in smiles:
-        mol = Chem.MolFromSmiles(elem)
-        moldata.append(mol)
+            row = np.array([desc_MolWt,
+                            desc_MolLogP,
+                            desc_NumHDonors,
+                            desc_NumHAcceptor])
 
-    baseData = []
-
-    for mol in moldata:
-        desc_MolWt = Descriptors.MolWt(mol)
-        desc_MolLogP = Descriptors.MolLogP(mol)
-        desc_NumHDonors = Descriptors.NumHDonors(mol)
-        desc_NumHAcceptor = Descriptors.NumHAcceptors(mol)
-
-        row = np.array([desc_MolWt,
-                        desc_MolLogP,
-                        desc_NumHDonors,
-                        desc_NumHAcceptor])
-
-        baseData.append(row)
-
-    columnnames = ['MW', 'LogP', 'NumHDonors', 'NumHAcceptors']
-    descriptors = pd.DataFrame(data=baseData, columns=columnnames)
-
-    return descriptors
-
-def combine_dataframes(df1,df2):
-    """
-    :param df1: First dataframe
-    :param df2: Second dataframe
-    :return: Combine two dataframe column-wise
-    """
-    df_combined = pd.concat([df1, df2], axis=1)
-    return df_combined
-
-def subset_df_for_lipinski(df):
-    """
-    :param df: The dataframe to subset
-    :return: Subset data frame based on Lipinski rules
-    and bioactivity
-    """
-    df_sub_lipinski = df[
-        (df['MW'] <= 500) &
-        (df['LogP'] <= 5) &
-        (df['NumHDonors'] <= 5) &
-        (df['NumHAcceptors'] <= 10) &
-        (df['bioactivity_class'] == 'active')]
-
-    df_sub_lipinski = df_sub_lipinski.reset_index(drop=True)
-    return df_sub_lipinski
+            self.baseData.append(row)
+        self.descriptors = pd.DataFrame(data=self.baseData, columns=self.columnnames)
+        
+    def combine_dataframes(self, df2):
+        df_combined = pd.concat([self.descriptors, df2], axis=1)
+        return df_combined
+    
+    def subset_df_for_lipinski(self, df):
+        df_sub_lipinski = df[
+            (df['MW'] <= 500) &
+            (df['LogP'] <= 5) &
+            (df['NumHDonors'] <= 5) &
+            (df['NumHAcceptors'] <= 10) &
+            (df['bioactivity_class'] == 'active')]
+        df_sub_lipinski = df_sub_lipinski.reset_index(drop=True)
+        return df_sub_lipinski
